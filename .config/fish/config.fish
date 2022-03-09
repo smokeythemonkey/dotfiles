aaa50421676c226c400ba871f4106c4f5275eb3b
awollgarten <awollgarten@posteo.net>
2022-03-09 19:36:07 +0100

    added aliases to fish, added keybindings for moving workspace to different monitor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,13 +4,21 @@ end
 
 
 # init asdf-vm
-#source ~/.asdf/asdf.fish
+source ~/.asdf/asdf.fish
 
 # init starship prompt
-#starship init fish | source
+starship init fish | source
 
 # direnv hook
-# direnv hook fish | source
+direnv hook fish | source
 
 # Created by `pipx` on 2022-01-16 14:58:58
 set PATH $PATH /home/awollgarten/.local/bin
+
+# Aliases
+alias rmi="rm -i"
+alias aptud="sudo apt update"
+alias aptug="sudo apt upgrade -y"
+alias apti="sudo apt -y install "
+alias aptr="sudo apt -y remove "
+alias ls="ls -lah --color"
