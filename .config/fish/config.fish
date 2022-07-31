9edfd0ba1d83353e30dc0cdef0cde9360bcba53b
awollgarten <awollgarten@posteo.net>
2022-07-31 20:44:39 +0200

    add regolith2, update fish abbr and add shell icon in starship prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -U fish_greeting "🐟"
 
 # init asdf-vm
 source ~/.asdf/asdf.fish
@@ -19,25 +20,32 @@ set PATH $PATH /home/awollgarten/.local/bin
 set -x PYTHONPATH '/home/awollgarten/.local/pipx/venvs/pdm/lib/python3.10/site-packages/pdm/pep582' $PYTHONPATH
 
 # Abbreviations
-abbr -a rmi rm -i
-abbr -a rmf rm -rf
-abbr -a ls ls -lah --color
-abbr -a df df -H
+abbr rmi rm -i
+abbr rmf rm -rf
+abbr ls ls -lah --color
+abbr df df -H
 
 # Apt
-abbr -a aptud sudo apt update
-abbr -a aptug sudo apt upgrade -y
-abbr -a aptin sudo apt -y install 
-abbr -a aptrm sudo apt -y remove 
-abbr -a aptls apt list --upgradable
+abbr aptud sudo apt update
+abbr aptug sudo apt upgrade -y
+abbr aptin sudo apt -y install 
+abbr aptrm sudo apt -y remove 
+abbr aptls apt list --upgradable
+abbr aptpg sudo apt purge
+abbr aptar sudo apt autoremove
+abbr aptac apt autoclean
 
 # Docker containers
 # dfu-programmer
-abbr -a dfuerase dfu-programmer atmega32u4 erase
-abbr -a dfuflash dfu-programmer atmega32u4 flash
+abbr dfuerase dfu-programmer atmega32u4 erase
+abbr dfuflash dfu-programmer atmega32u4 flash
 
 # SSH
 alias sshhosts='grep -P "^Host" $HOME/.ssh/config'
 
 # Disk
 alias disks='lsblk -o HOTPLUG,NAME,SIZE,MODEL,TYPE | awk "NR == 1 || /disk/"'
+
+# Window Class
+alias window-class="xprop"
+
