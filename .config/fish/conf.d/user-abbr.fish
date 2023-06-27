5bdcc1a700346db8dec001232d81dd0ab2b1474c
awollgarten <awollgarten@posteo.net>
2023-06-27 20:46:14 +0200

    add zellij layouts, update bootstrap, move abbr from config.fish to conf.d, update terminal font

---
+++ .config/fish/conf.d/user-abbr.fish
@@ -0,0 +1,63 @@
+# Abbreviations and Aliases
+abbr rmi rm -i
+abbr rmf rm -rf
+
+# abbr ls exa --time-style=long-iso --group-directories-first --icons --no-permissions --no-user -l
+alias ls="exa"
+alias l="exa -l --all --group-directories-first --icons --time-style=long-iso"
+alias ll="exa -l --all --all --group-directories-first --icons --time-style=long-iso"
+alias lt="exa -T --level=2 --group-directories-first"
+alias llt="exa -lT --level=2 --group-directories-first"
+alias lT="exa -T --level=4 --group-directories-first"
+
+abbr df df -H
+abbr du dust
+
+abbr asdf rtx
+
+
+# system update
+alias system-update="sudo nala update && sudo nala upgrade && pacstall -Up"
+
+# Nala
+alias apt="nala"
+alias aptud="sudo nala update && nala list --upgradeable"
+abbr aptls nala list --upgradeable
+abbr aptug sudo nala upgrade -y
+abbr aptin sudo nala install -y
+abbr aptrm sudo nala remove -y
+abbr aptpg sudo nala purge
+abbr aptar sudo nala autoremove
+abbr aptac nala autoclean
+
+# linode cli
+abbr lin linode-cli
+
+# dfu-programmer
+abbr dfuerase dfu-programmer atmega32u4 erase
+abbr dfuflash dfu-programmer atmega32u4 flash
+abbr qmkstm compile -kb lily58 -km smokeythemonkey
+
+# SSH
+alias sshhosts='rg "^Host" $HOME/.ssh/config'
+abbr sshkeygen ssh-keygen -t ed25519 (whoami)@(hostname)-(date -I) -f ~/.ssh/
+
+# Disk
+alias disks='lsblk -o HOTPLUG,NAME,SIZE,MODEL,TYPE | awk "NR == 1 || /disk/"'
+
+# Window Class
+alias window-class="xprop"
+
+# CLI tools
+alias cat="bat"
+
+
+# web-search
+alias ddg="web-search duckduckgo"
+alias sof="web-search stackoverflow"
+alias yts="web-search youtube"
+
+# zellij 
+abbr zz zellij
+alias zp="zellij action new-tab -l ~/.config/zellij/layouts/project_explorer.kdl"
+alias zmon="zellij action new-tab -l ~/.config/zellij/layouts/monitor.kdl"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 # set -Ux WEZTERM_CONFIG_FILE '$HOME/.config/wezterm/wezterm.lua'
 if status is-interactive
-#  eval (zellij setup --generate-auto-start fish | string collect)
+  eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 set -U fish_greeting
@@ -15,56 +15,6 @@ starship init fish | source
 # Created by `pipx` on 2022-01-16 14:58:58
 set PATH $PATH $HOME/.local/bin
 
-# Abbreviations
-abbr rmi rm -i
-abbr rmf rm -rf
-
-abbr ls exa --time-style=long-iso --group-directories-first --icons --no-permissions --no-user -l
-abbr df df -H
-abbr du dust
-
-abbr asdf rtx
-abbr zz zellij
-
-# system update
-alias system-update="sudo nala update && sudo nala upgrade && pacstall -Up"
-
-# Nala
-alias apt="nala"
-alias aptud="sudo nala update && nala list --upgradeable"
-abbr aptls nala list --upgradeable
-abbr aptug sudo nala upgrade -y
-abbr aptin sudo nala install -y
-abbr aptrm sudo nala remove -y
-abbr aptpg sudo nala purge
-abbr aptar sudo nala autoremove
-abbr aptac nala autoclean
-
-# linode cli
-abbr lin linode-cli
-
-# dfu-programmer
-abbr dfuerase dfu-programmer atmega32u4 erase
-abbr dfuflash dfu-programmer atmega32u4 flash
-abbr qmkstm compile -kb lily58 -km smokeythemonkey
-
-# SSH
-alias sshhosts='rg "^Host" $HOME/.ssh/config'
-
-# Disk
-alias disks='lsblk -o HOTPLUG,NAME,SIZE,MODEL,TYPE | awk "NR == 1 || /disk/"'
-
-# Window Class
-alias window-class="xprop"
-
-# CLI tools
-alias cat="bat"
-
-
-# web-search
-alias ddg="web-search duckduckgo"
-alias sof="web-search stackoverflow"
-alias yts="web-search youtube"
 
 rtx activate fish | source
 fish_add_path $HOME/.local/share/rtx/shims
