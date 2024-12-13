299aef660fe654d0fbbff63e8e2f71a28195712b
a.wollgarten <awollgarten@posteo.net>
2024-12-13 21:09:32 +0100

    feat: add run once after script

---
+++ .chezmoiscripts/linux/run_once_after_01-install-packages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2312
+set -e
+LINE="-------------------------------------------"
+
+echo "Installing atuin"
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+echo $LINE
+
+echo "Installing mise"
+curl https://mise.run | sh
+
+echo $LINE
+
+echo "Installing fisher"
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+echo $LINE
--- .chezmoiscripts/linux/run_once_before_01-configure-repos.sh
+++ .chezmoiscripts/linux/run_once_before_01-configure-repos.sh
@@ -15,38 +15,42 @@ if command -v apt &>/dev/null; then
     curl https://gitlab.com/volian/volian-archive/-/raw/main/install-nala.sh | bash
     sudo apt install -t nala nala
   fi
-
-  echo "Setting up flathub"
-  flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
   sudo nala fetch --auto -y --https-only --non-free
   sudo nala install --update -y curl wget git ca-certificates libfuse2 gnupg2
 
-  echo "Installing NF"
-  curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
-
-  getnf -i FiraCode FiraMono UbuntuMono UbuntuSans ZedMono
-
-  echo "Adding eza repo"
-  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-
-  echo "Adding alacritty ppa"
-  sudo add-apt-repository ppa:aslatter/ppa -y
-
-  echo "Adding syncthing ppa"
-  sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-  echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-  echo "Adding neovim ppa"
-  sudo add-apt-repository ppa:neovim-ppa/stable
-
-  echo "Installing atuin"
-  curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-
-  echo "Installing uv"
-  curl -LsSf https://astral.sh/uv/install.sh | sh
-
 fi
+
+echo $LINE
+
+echo "Setting up flathub"
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo $LINE
+
+echo "Installing NF"
+curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+
+getnf -i FiraCode FiraMono UbuntuMono UbuntuSans ZedMono
+
+echo $LINE
+
+echo "Adding eza repo"
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg -y
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+echo "Adding alacritty ppa"
+sudo add-apt-repository ppa:aslatter/ppa -y
+
+echo "Adding syncthing ppa"
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+echo "Adding neovim ppa"
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+
+echo "Adding fish ppa"
+sudo apt-add-repository ppa:fish-shell/release-3 -y
+
+echo $LINE
