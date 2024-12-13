886ced02ba53292ec51ca799cfdfb1279c500b29
a.wollgarten <awollgarten@posteo.net>
2024-12-13 21:38:42 +0100

    chore: update configs

--- .chezmoiscripts/linux/run_once_before_01-configure-repos.sh
+++ .chezmoiscripts/linux/run_once_before_01-configure-repos.sh
@@ -35,7 +35,7 @@ getnf -i FiraCode FiraMono UbuntuMono UbuntuSans ZedMono
 echo $LINE
 
 echo "Adding eza repo"
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg -y
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 
--- one-time-repo.md
+++
@@ -1,6 +0,0 @@
-# one time setup repo
-
-[x] eza
-[x] atuin
-- set default Terminal alacritty
-[x] curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash`
