5ca375ee800466a7d02b52f9e11cdc1edb021154
a.wollgarten <a.wollgarten@brainsuite.ai>
2025-01-02 09:32:37 +0100

    feat: update fish pypi index function, git config

--- private_dot_config/fish/functions/readonly_setup-internal-pypi.fish
+++ private_dot_config/fish/functions/readonly_setup-internal-pypi.fish
@@ -1,6 +1,5 @@
 function setup-internal-pypi --description="Setup Python tools with new Codeartifact auth token"
     set -e PIP_EXTRA_INDEX_URL
-    set -e PDM_PUBLISH_PASSWORD
     set -e TWINE_PASSWORD
     set -e INDEX_PASSWORD
 
@@ -10,6 +9,8 @@ function setup-internal-pypi --description="Setup Python tools with new Codearti
 
     set -Ux INDEX_USERNAME aws
     set -Ux INDEX_PASSWORD $CODEARTIFACT_AUTH_TOKEN
+
+    set -Ux UV_EXTRA_INDEX_URL "https://aws:$CODEARTIFACT_AUTH_TOKEN@aimpower-345335739971.d.codeartifact.eu-central-1.amazonaws.com/pypi/internal-packages/simple/"
 
 
     echo Successfully setup aimpower internal-packages pypi index and repo
