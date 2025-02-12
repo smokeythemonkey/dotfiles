efccc8a9b031401657216057d22d296d187dcd33
a.wollgarten <a.wollgarten@brainsuite.ai>
2025-02-12 07:23:37 +0100

    feat: update setup-pypi and update mise

--- private_dot_config/fish/functions/readonly_setup-internal-pypi.fish
+++ private_dot_config/fish/functions/readonly_setup-internal-pypi.fish
@@ -1,17 +1,18 @@
 function setup-internal-pypi --description="Setup Python tools with new Codeartifact auth token"
     set -e PIP_EXTRA_INDEX_URL
     set -e TWINE_PASSWORD
+    set -e TWINE_REPOSITORY_URL
+    set -e TWINE_REPOSITORY_URL
     set -e INDEX_PASSWORD
-
-    set -Ux TWINE_REPOSITORY_URL "https://aimpower-345335739971.d.codeartifact.eu-central-1.amazonaws.com/pypi/internal-packages/"
-    set -Ux TWINE_USERNAME aws
-    set -Ux TWINE_PASSWORD $CODEARTIFACT_AUTH_TOKEN
+    set -e UV_EXTRA_INDEX_URL
+    set -e UV_INDEX
 
     set -Ux INDEX_USERNAME aws
     set -Ux INDEX_PASSWORD $CODEARTIFACT_AUTH_TOKEN
 
-    # set -Ux UV_EXTRA_INDEX_URL "https://aws:$CODEARTIFACT_AUTH_TOKEN@aimpower-345335739971.d.codeartifact.eu-central-1.amazonaws.com/pypi/internal-packages/simple/"
-
+    set -Ux UV_INDEX "https://aws:$CODEARTIFACT_AUTH_TOKEN@aimpower-345335739971.d.codeartifact.eu-central-1.amazonaws.com/pypi/internal-packages/simple/"
+    set -Ux UV_DEFAULT_INDEX "https://pypi.org/simple"
+    set -Ux UV_NATIVE_TLS true
 
     echo Successfully setup aimpower internal-packages pypi index and repo
 end
