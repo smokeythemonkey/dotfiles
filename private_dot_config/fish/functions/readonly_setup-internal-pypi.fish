2a4f6ac29b3ab389b3ef8ef847b8b6535b7e4347
a.wollgarten <a.wollgarten@brainsuite.ai>
2025-03-06 09:11:08 +0100

    chore: update fish

--- private_dot_config/fish/functions/readonly_setup-internal-pypi.fish
+++ private_dot_config/fish/functions/readonly_setup-internal-pypi.fish
@@ -10,8 +10,8 @@ function setup-internal-pypi --description="Setup Python tools with new Codearti
     set -Ux INDEX_USERNAME aws
     set -Ux INDEX_PASSWORD $CODEARTIFACT_AUTH_TOKEN
 
-    set -Ux UV_INDEX "https://aws:$CODEARTIFACT_AUTH_TOKEN@aimpower-345335739971.d.codeartifact.eu-central-1.amazonaws.com/pypi/internal-packages/simple/"
-    set -Ux UV_DEFAULT_INDEX "https://pypi.org/simple"
+    # set -Ux UV_INDEX "https://aws:$CODEARTIFACT_AUTH_TOKEN@aimpower-345335739971.d.codeartifact.eu-central-1.amazonaws.com/pypi/internal-packages/simple/"
+    # set -Ux UV_DEFAULT_INDEX "https://pypi.org/simple"
     set -Ux UV_NATIVE_TLS true
 
     echo Successfully setup aimpower internal-packages pypi index and repo
